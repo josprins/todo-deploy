1213e65988d9fa2cee20351292bce73e1594f560
josprins <jco.prins@gmail.com>
2019-09-11 16:05:07 +0200

    Updates

---
+++ precache-manifest.bcc3cd69f1f8f9131a4638144cdf695f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f995eb6de21a79a959dbb7162bf94a00",
+    "url": "/todo-deploy/index.html"
+  },
+  {
+    "revision": "fbcb07560d5e5fa49f5b",
+    "url": "/todo-deploy/static/css/main.62fb3c60.chunk.css"
+  },
+  {
+    "revision": "4a06ac2e3984685c9eff",
+    "url": "/todo-deploy/static/js/2.8a6d9bb0.chunk.js"
+  },
+  {
+    "revision": "fbcb07560d5e5fa49f5b",
+    "url": "/todo-deploy/static/js/main.3806a1dd.chunk.js"
+  },
+  {
+    "revision": "e2a8716bd7dc982126cd",
+    "url": "/todo-deploy/static/js/runtime~main.62cd571d.js"
+  }
+]);
